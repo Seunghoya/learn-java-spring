272dd803ea45e78b1f2fa2525ccfca917b680199
Seunghoya <shan@ntllab.com>
2022-08-03 17:01:22 +0900

    회원 도메인 테스트

--- src/main/java/learnjavaspring/core/member/MemberServiceImpl.java
+++ src/main/java/learnjavaspring/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package learnjavaspring.core.member;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // MemberRepository 인터페이스를 의존하지만 new MemmoryMemberRepository는 구현체를 의존하는 문제가 있다.
 
     @Override
     public void join(Member member) {
