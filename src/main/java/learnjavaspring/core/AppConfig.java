a88a846846496d6e5f6bb81f036044ee3ade4802
Seunghoya <shan@ntllab.com>
2022-08-04 12:25:33 +0900

    Spring 적용

--- src/main/java/learnjavaspring/core/AppConfig.java
+++ src/main/java/learnjavaspring/core/AppConfig.java
@@ -9,25 +9,30 @@ import learnjavaspring.core.member.MemberServiceImpl;
 import learnjavaspring.core.member.MemoryMemberRepository;
 import learnjavaspring.core.order.OrderService;
 import learnjavaspring.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // 애플리케이션에서 실제 동작이 필요한 구현 객체를 생성
     // ex) MemberServiceImpl, OrderServiceImpl, MemoryMemberRepository, FixDiscountPolicy ...
 
     // 생성한 객체 인스턴스의 참조를 생성자를 통해 주입해준다.
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         // 할인 정책을 변경하기 위해서 코드 단 한 줄만 수정하면 된다.
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
--- src/main/java/learnjavaspring/core/MemberApp.java
+++ src/main/java/learnjavaspring/core/MemberApp.java
@@ -4,13 +4,23 @@ import learnjavaspring.core.member.Grade;
 import learnjavaspring.core.member.Member;
 import learnjavaspring.core.member.MemberService;
 import learnjavaspring.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 //        MemberService memberService = new MemberServiceImpl();
         // 이제 memberService는 AppConfig에서 가져와야 한다.
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        Member member = new Member(1L, "memberA", Grade.VIP);
+//        memberService.join(member);
+
+        // Spring 적용
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
