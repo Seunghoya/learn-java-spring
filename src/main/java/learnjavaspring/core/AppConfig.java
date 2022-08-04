c9fdaf6832082f564f683a34a70cd8af1bc975ae
Seunghoya <shan@ntllab.com>
2022-08-04 12:02:32 +0900

    appconfig 리팩토링

--- src/main/java/learnjavaspring/core/AppConfig.java
+++ src/main/java/learnjavaspring/core/AppConfig.java
@@ -1,6 +1,9 @@
 package learnjavaspring.core;
 
+import learnjavaspring.core.discount.DiscountPolicy;
 import learnjavaspring.core.discount.FixDiscountPolicy;
+import learnjavaspring.core.discount.RateDiscountPolicy;
+import learnjavaspring.core.member.MemberRepository;
 import learnjavaspring.core.member.MemberService;
 import learnjavaspring.core.member.MemberServiceImpl;
 import learnjavaspring.core.member.MemoryMemberRepository;
@@ -12,12 +15,21 @@ public class AppConfig {
     // ex) MemberServiceImpl, OrderServiceImpl, MemoryMemberRepository, FixDiscountPolicy ...
 
     // 생성한 객체 인스턴스의 참조를 생성자를 통해 주입해준다.
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    private DiscountPolicy discountPolicy() {
+        // 할인 정책을 변경하기 위해서 코드 단 한 줄만 수정하면 된다.
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
 }
